3d7cc7d02a7cac526699a6536e07f9219e1568db
ferrariflaviaa <flaviaferrarit@gmail.com>
2022-03-02 21:31:13 -0300

    configuring modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,24 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import logoImg from '../../assets/logo.svg'
-
 import { Container, Content } from './styles'
 
-export function Header(){
+//PROPRIEDADES QUE HEADER PODE RECEBER:
+interface HeaderProps{
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({onOpenNewTransactionModal}: HeaderProps){
+
     return(
         <Container>
             <Content>
                 <img src={logoImg} alt="Logo da aplicação DT Money" />
-                <button type="button">
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova transação
                 </button>
+
+
             </Content>
         </Container>
     )
