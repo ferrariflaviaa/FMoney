613e0ab814c808863787420fe79f3ec6d214e400
ferrariflaviaa <flaviaferrarit@gmail.com>
2022-03-05 19:05:57 -0300

    List of transactions and seeds

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,29 @@ createServer({
   models:{
     transaction: Model,
   },
+
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000, 
+          createAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'casa',
+          amount: 1100, 
+          createAt: new Date('2021-02-14 11:00:00'),
+        }
+      ],
+    })
+  },
   routes() {
     //AQUI FAZENDO DIRECIONAMOS TODAS CHAMADAS QUE TEM API:
     this.namespace = 'api';
