dba7375f268dc489ac5c6053b544d9efaba667e4
ferrariflaviaa <flaviaferrarit@gmail.com>
2022-03-06 14:22:52 -0300

    Calculating summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -8,7 +8,33 @@ import { Container } from "./styles";
 
 export function Summary() {
     const { transactions } = useContext(TransactionsContext);
-    
+
+    // const totalDeposits = transactions.reduce ((acc, transactions) => {
+    //     if (transactions.type ==='deposit') {
+    //         return acc + transactions.amount;
+    //     }
+    //     return acc;
+    // },0);
+
+    //CALCULANDO AS ENTRADAS E SAÍDAS:
+    const summary = transactions.reduce((acc, transaction) => {
+        if (transaction.type === 'deposit') {
+            acc.deposits += transaction.amount;
+            acc.total += transaction.amount;
+
+        } else {
+            acc.withdraws += transaction.amount;
+            acc.total -= transaction.amount;
+        }
+
+        return acc;
+    }, {
+        deposits: 0,
+        withdraws: 0,
+        total: 0,
+
+    })
+
     return (
         <Container>
             <div>
@@ -16,7 +42,12 @@ export function Summary() {
                     <p>Entradas</p>
                     <img src={incomeImg} alt="Entradas" />
                 </header>
-                    <strong>R$1000,00</strong>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.deposits)}
+                </strong>
             </div>
 
             <div>
@@ -24,7 +55,12 @@ export function Summary() {
                     <p>Saídas</p>
                     <img src={outcomeImg} alt="Saídas" />
                 </header>
-                <strong> - R$500,00</strong>
+                <strong>
+                    - {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.withdraws)}
+                </strong>
             </div>
 
             <div className="highlight-background">
@@ -32,7 +68,12 @@ export function Summary() {
                     <p>Total</p>
                     <img src={totalImg} alt="Total" />
                 </header>
-                    <strong>R$500,00</strong>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.total)}
+                </strong>
             </div>
         </Container>
     )
