3b31e02935818011eb1aad5b91bb5b3412e4072d
ferrariflaviaa <flaviaferrarit@gmail.com>
2022-03-05 21:24:15 -0300

    loading transactions

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {GlobalStyle} from "./styles/global"
 import Modal from 'react-modal';
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./TransactionsContext";
 
 Modal.setAppElement('#root');
 
@@ -21,12 +22,16 @@ function App() {
     setIsNewTransactionModalOpen(false);
   }
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
       <Dashboard/>
+
       <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
+
       <GlobalStyle/>
-    </>
+
+    </TransactionsProvider>
   );
 }
 
